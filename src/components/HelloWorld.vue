ee9b2027caef64893a851ad0e601e04473a899c2
黄振鹏 <hrrmzp@163.com>
2018-04-08 18:13:44 +0800

    测试vant
    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+	<div>
+	  <div class="hello">
+	    <van-tabbar v-model="active">
+			  <van-tabbar-item icon="shop">标签</van-tabbar-item>
+			  <van-tabbar-item icon="chat" dot>标签</van-tabbar-item>
+			  <van-tabbar-item icon="records" info="5">标签</van-tabbar-item>
+			  <van-tabbar-item icon="gold-coin" info="20">标签</van-tabbar-item>
+			</van-tabbar>
+	  </div>
+	  <div class="box">
+	  	<h1 class="title">hello</h1>
+	  </div>
   </div>
 </template>
 
@@ -88,26 +19,21 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      active: 0
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped lang="less">
+.box{
+	width: 80%;
+	height: 200px;
+	margin: 0 auto;
+	background-color: #ccc;
+	.title{
+		color: #f00;
+	}
 }
 </style>
